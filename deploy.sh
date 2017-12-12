3cffd344cf2b822980e56a1f1e1dfcdfcc27057b
drake-jin <dydwls121200@gmail.com>
2017-12-13 06:53:30 +0900

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+git checkout -b gh-pages
+git rm -rf .
+git commit -am "First commit to gh-pages branch"
+git push origin gh-pages
+git checkout master
+git push origin `git subtree split --prefix _site gh-pages`:gh-pages --force
+
 # make `_sites`
 bundle exec jekyll build
 git subtree push --prefix _site origin gh-pages
